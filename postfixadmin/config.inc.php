d3c4c95fef62f508349a8f4def150d79ee41f4a2
appdesign1987 <jeroen@jeroenvd.nl>
2020-04-06 19:32:17 +0200

    Update config.inc.php

--- postfixadmin/config.inc.php
+++ postfixadmin/config.inc.php
@@ -80,7 +80,7 @@ function language_hook($PALANG, $language) {
 // mysql = MySQL 3.23 and 4.0, 4.1 or 5
 // mysqli = MySQL 4.1+ or MariaDB
 // pgsql = PostgreSQL
-$CONF['database_type'] = 'mysql';
+$CONF['database_type'] = 'mysqli';
 $CONF['database_host'] = 'mariadb';
 $CONF['database_user'] = 'mail';
 $CONF['database_password'] = 'mail';
